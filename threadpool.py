acea412e0d116e60d370c95c511fe3ffbb2397d7
Jan Veverka <jan.veverka@cern.ch>
2015-02-18 17:46:10 +0100

    Add prototype driver and sleeper

--- threadpool.py
+++ threadpool.py
@@ -1,3 +1,5 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
 '''
 This is a test to understand the behaviour of multiprocessing and it's
 ThreadPool.  It should answer these questions:
@@ -9,15 +11,58 @@ ThreadPool.  It should answer these questions:
 '''
 
 import logging
+import time
 logger = logging.getLogger(__name__)
 
-def main:
+#______________________________________________________________________________
+def main():
     logging.basicConfig(
         level=logging.DEBUG,
         format=r'%(asctime)s %(name)s %(levelname)s: %(message)s',
     )
-    logger.info('Start')
-    logger.info('Finished with great success!')
+    driver = Driver(nloops=3, pool_size=2)
+    driver.run()
 
+#______________________________________________________________________________
+class LoggingObject(object):
+    def __init__(self):
+        self.logger = logging.getLogger(type(self).__module__ + '.' +
+                                        type(self).__name__)
+
+#______________________________________________________________________________
+class Driver(LoggingObject):
+    def __init__(self, nloops=3, pool_size=2):
+        super(Driver, self).__init__()
+        self.nloops = nloops
+        self.pool_size = pool_size
+        self.work_slow = Sleeper(seconds_to_sleep = 1)
+        self.work_fast = Sleeper(seconds_to_sleep = 0.1)
+    def run(self):
+        self.logger.info(
+            'Starting %d loops with a pool of %d ...' % (
+                self.nloops, self.pool_size
+            )
+        )
+        for iloop in range(self.nloops):
+            self.logger.info('Loop %d' % iloop)
+            self.do_work(iloop)
+        self.logger.info('Finished running with great succes!')
+    def do_work(self, iloop):
+        self.work_fast('fast job %d' % iloop)
+        self.work_slow('slow job %d' % iloop)
+
+#______________________________________________________________________________
+class Sleeper(LoggingObject):
+    def __init__(self, seconds_to_sleep):
+        super(Sleeper, self).__init__()
+        self.seconds_to_sleep = seconds_to_sleep
+    def __call__(self, name=None):
+        if name:
+            self.logger.info('Starting %s ...' % name )
+        time.sleep(self.seconds_to_sleep)
+        if name:
+            self.logger.info('Finished %s.' % name )
+
+#______________________________________________________________________________
 if __name__ == '__main__':
     main()
